4ada7dbcf8439a60f2168bb43c4d52e602cae862
root <root@vagrant-centos65.vagrantup.com>
2014-11-07 15:33:04 +0900

    first commit

---
+++ site-cookbooks/httpd/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: httpd
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# http install
+
+package "httpd" do
+   action :install
+end
+
+
